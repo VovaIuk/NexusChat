5fca0efd7329d6e85a5ddd3b033792835dee6244
Vova student <gubob2015@gmail.com>
2025-12-17 23:42:24 +0300

    fix(gitignore): add file message.go

---
+++ backend/internal/adapter/postgres/message.go
@@ -0,0 +1,35 @@
+package postgres
+
+import (
+	"backend/internal/domain"
+	"context"
+	"fmt"
+)
+
+func Start() {
+
+}
+
+func (p *Pool) GetMessagesByChatID(ctx context.Context, chatId int) ([]domain.Message, error) {
+	var messages []domain.Message = make([]domain.Message, 0)
+	rows, err := p.pool.Query(ctx, `SELECT id, user_id, chat_id, text, time FROM messages WHERE chat_id = $1 ORDER BY time ASC`, chatId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query messages: %w", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var msg domain.Message
+		err := rows.Scan(&msg.ID, &msg.UserID, &msg.ChatID, &msg.Text, &msg.Time)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan message row: %w", err)
+		}
+		messages = append(messages, msg)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows iteration error: %w", err)
+	}
+
+	return messages, nil
+}
