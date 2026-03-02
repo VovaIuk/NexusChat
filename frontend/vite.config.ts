78087c46bfcb45da95cc7e06f30d40908ad3f010
Vova student <gubob2015@gmail.com>
2026-03-02 17:02:23 +0300

    fix allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // слушать на 0.0.0.0 — нужно для доступа из Docker
+    allowedHosts: [
+      'localhost',
+      'localhost:5173',
+      'xn----7sbbozvcgr0a7c4b.xn--p1ai',
+      'xn----7sbbozvcgr0a7c4b.xn--p1ai:5173',
+    ],
   },
 })
